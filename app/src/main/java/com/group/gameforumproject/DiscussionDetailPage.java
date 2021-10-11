e385e4204475dc86a48e172a442dcf984b9b4a44
Thelango99 <sanderrf@stud.ntnu.no>
2021-10-11 10:57:25 +0200

    Added a few more classes

---
+++ app/src/main/java/com/group/gameforumproject/DiscussionDetailPage.java
@@ -0,0 +1,14 @@
+package com.group.gameforumproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiscussionDetailPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discussion_detail_page);
+    }
+}

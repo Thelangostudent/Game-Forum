4c3e829047ae007b03348e3f5f5f54cb0d4e182d
Thelango99 <sanderrf@stud.ntnu.no>
2021-10-18 10:08:02 +0200

    changed newsActivity from recyclerview to standard text fields to aid in project simplicity and reduce amount of potential classes.

--- app/src/main/java/com/group/gameforumproject/NewsActivity.java
+++ app/src/main/java/com/group/gameforumproject/NewsActivity.java
@@ -3,12 +3,28 @@ package com.group.gameforumproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.TextView;
+/**
+ * This newsClass fetches the latest news from the server
+ *
+ * @author Gruppe 2
+ * @version 0.1 Aplha
+ *
+ * */
 public class NewsActivity extends AppCompatActivity {
+
+    TextView latestNewsTitle;
+    TextView latestNewsDescription;
+    TextView latestNewsAuthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        //fields for the news article
+        latestNewsTitle = findViewById(R.id.txt_LatestNewsTitle);
+        latestNewsDescription = findViewById(R.id.txt_LatestNewsDescription);
+        latestNewsAuthor = findViewById(R.id.txt_latestNewsAuthor);
     }
 }

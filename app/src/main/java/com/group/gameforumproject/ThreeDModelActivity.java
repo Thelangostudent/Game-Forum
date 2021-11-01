2dd90b5c0d69cf2ffaf298c683a8ff1f53dad6ab
Thelango99 <sanderrf@stud.ntnu.no>
2021-11-01 12:38:38 +0100

    Added 3d Modelviewer to 3D models page.

--- app/src/main/java/com/group/gameforumproject/ThreeDModelActivity.java
+++ app/src/main/java/com/group/gameforumproject/ThreeDModelActivity.java
@@ -2,13 +2,33 @@ package com.group.gameforumproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThreeDModelActivity extends AppCompatActivity {
+
+    Button shipModelButton;
+    Button playercharacterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three_dmodel);
+
+        shipModelButton = findViewById(R.id.btn_IngameShipModel);
+        playercharacterButton = findViewById(R.id.btn_IngameShipModel);
+
+        shipModelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+                sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/mainShipRaw.gltf"));
+                sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
+                startActivity(sceneViewerIntent);
+            }
+        });
     }
 }

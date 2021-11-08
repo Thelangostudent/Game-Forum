8a4ca53a479e178eaa52288518015827b65b4458
Rorvik <stianlr@stud.ntnu.no>
2021-11-08 21:56:22 +0100

    added code for player/asteroid 3D models, uploaded the exported 3D models to the alternative github repo (the one used for 3D models)

--- app/src/main/java/com/group/gameforumproject/ThreeDModelActivity.java
+++ app/src/main/java/com/group/gameforumproject/ThreeDModelActivity.java
@@ -11,7 +11,9 @@ import android.widget.Button;
 public class ThreeDModelActivity extends AppCompatActivity {
 
     Button shipModelButton;
-    Button playercharacterButton;
+    Button playerCharacterButton;
+    Button asteroidButton;
+
     Button logoutButton;
 
     @Override
@@ -20,13 +22,35 @@ public class ThreeDModelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_three_dmodel);
 
         shipModelButton = findViewById(R.id.btn_IngameShipModel);
-        playercharacterButton = findViewById(R.id.btn_IngameShipModel);
+        playerCharacterButton = findViewById(R.id.btn_playerCharacterModel);
+        asteroidButton = findViewById(R.id.btn_asteroid);
+
 
         shipModelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
                 sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/mainShipRaw.gltf"));
+                sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
+                startActivity(sceneViewerIntent);
+            }
+        });
+
+        playerCharacterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+                sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/player.gltf"));
+                sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
+                startActivity(sceneViewerIntent);
+            }
+        });
+
+        asteroidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+                sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/asteroid.gltf"));
                 sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
                 startActivity(sceneViewerIntent);
             }

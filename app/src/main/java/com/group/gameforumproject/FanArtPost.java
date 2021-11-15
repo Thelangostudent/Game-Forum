9dbf981931f71171781bf1061edc38a47cd1ff26
Mamo <mahmodahmed2000@gmail.com>
2021-11-15 11:33:51 +0100

    added FanArtComments class

--- app/src/main/java/com/group/gameforumproject/FanArtPost.java
+++ app/src/main/java/com/group/gameforumproject/FanArtPost.java
@@ -1,11 +1,15 @@
 package com.group.gameforumproject;
 
+import com.google.firebase.database.ServerValue;
+
 public class FanArtPost {
 
     private int id;
+    private String postKey;
     private String title;
     private User user;
     private String image;
+    private Object timestamp;
 
     public FanArtPost (int id, String title, User user, String image )
     {
@@ -13,6 +17,17 @@ public class FanArtPost {
         this.title = title;
         this.user = user;
         this.image = image;
+        this.timestamp = ServerValue.TIMESTAMP;
+    }
+
+    public FanArtPost(){}
+
+    public String getPostKey() {
+        return postKey;
+    }
+
+    public void setPostKey(String postKey) {
+        this.postKey = postKey;
     }
 
     public int getId() {

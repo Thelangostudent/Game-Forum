31af11998f40e0a95c76f8b1d2b4d16b895e07ad
Thelango99 <sanderrf@stud.ntnu.no>
2021-11-15 09:42:24 +0100

    Fleshed out FanartPost class (undocumented)

--- app/src/main/java/com/group/gameforumproject/FanArtPost.java
+++ app/src/main/java/com/group/gameforumproject/FanArtPost.java
@@ -3,9 +3,47 @@ package com.group.gameforumproject;
 public class FanArtPost {
 
     private int id;
-    private String Title;
+    private String title;
     private User user;
     private String image;
 
+    public FanArtPost (int id, String title, User user, String image )
+    {
+        this.id = id;
+        this.title = title;
+        this.user = user;
+        this.image = image;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }

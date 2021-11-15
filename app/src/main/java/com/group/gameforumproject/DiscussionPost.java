cb4318cf73a78b53805dcde11aa2d74dac96876d
Thelango99 <sanderrf@stud.ntnu.no>
2021-11-15 09:10:24 +0100

    Fleshed out Discussion class

--- app/src/main/java/com/group/gameforumproject/DiscussionPost.java
+++ app/src/main/java/com/group/gameforumproject/DiscussionPost.java
@@ -1,4 +1,32 @@
 package com.group.gameforumproject;
 
 public class DiscussionPost {
+
+    private int id;
+    public String title;
+    public String description;
+    public Comments discussionComments;
+
+
+    public DiscussionPost (int id, String title, String description, Comments discussionComments)
+    {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.discussionComments = discussionComments;
+
+    }
+
+    /**
+     * returns the type of post this class is.
+     * Used for comments to separate FanartComments and DiscussionComments
+     * 'D' = discussiontype and 'F' = FanArtType;
+     *
+     * */
+    public char postType()
+    {
+        char postype = 'D';
+
+        return postype;
+    }
 }

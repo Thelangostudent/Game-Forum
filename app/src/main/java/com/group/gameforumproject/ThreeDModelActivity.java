2745f79b059169ccfa9babbadb8feb06754712ac
Rorvik <stianlr@stud.ntnu.no>
2021-11-15 12:23:31 +0100

    Updated 3D Model viewer UI, added glide, added icons to models, added dropdown menu and corresponding XML files

--- app/src/main/java/com/group/gameforumproject/ThreeDModelActivity.java
+++ app/src/main/java/com/group/gameforumproject/ThreeDModelActivity.java
@@ -2,20 +2,22 @@ package com.group.gameforumproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.bumptech.glide.Glide;
 
 public class ThreeDModelActivity extends AppCompatActivity {
 
-    Button shipModelButton;
-
-    Button playerCharacterButton;
-    Button asteroidButton;
-
-
+    Button viewModelButton;
     Button logoutButton;
 
     @Override
@@ -23,39 +25,41 @@ public class ThreeDModelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three_dmodel);
 
-        shipModelButton = findViewById(R.id.btn_IngameShipModel);
-        playerCharacterButton = findViewById(R.id.btn_playerCharacterModel);
-        asteroidButton = findViewById(R.id.btn_asteroid);
+        Context context = this;
+
         logoutButton= findViewById(R.id.btn_3dModelLogout_);
+        viewModelButton= findViewById(R.id.btn_viewModelButton);
 
+        Spinner spinner=findViewById(R.id.spinner);
+        int singleItem = R.layout.spinner_dropdown_item;
 
-        shipModelButton.setOnClickListener(new View.OnClickListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
-                sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/mainShipRaw.gltf"));
-                sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
-                startActivity(sceneViewerIntent);
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                updatePicture(context);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                System.out.println("Nothing is currently selected");
             }
         });
 
-        playerCharacterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
-                sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/player.gltf"));
-                sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
-                startActivity(sceneViewerIntent);
-            }
-        });
+        ImageView previewImage=(ImageView) findViewById(R.id.previewImage);
 
-        asteroidButton.setOnClickListener(new View.OnClickListener() {
+        Glide.with(context)
+                .load("https://raw.githubusercontent.com/Thelangostudent/Models/master/Pictures/none.png")
+                .into(previewImage);
+
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(context, R.array.models, singleItem);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        viewModelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
-                sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/asteroid.gltf"));
-                sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
-                startActivity(sceneViewerIntent);
+                startModelViewer();
             }
         });
 
@@ -66,11 +70,68 @@ public class ThreeDModelActivity extends AppCompatActivity {
                 startActivity(mainpageIntent);
             }
         });
+    }
 
 
+    public void startModelViewer(){
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        String text = mySpinner.getSelectedItem().toString();
+
+        String selectedModel = "none";
+        switch (text) {
+            case "Ship":
+                selectedModel = "mainShipRaw";
+                break;
+            case "Player":
+                selectedModel = "player";
+                break;
+            case "Asteroid":
+                selectedModel = "asteroid";
+                break;
+            case "Forest Village Planet":
+                selectedModel = "forestVillage";
+                break;
+            case "Select model":
+                selectedModel = "none";
+                break;
+        }
+
+        if (!selectedModel.equals("none")){
+            Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+            sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/Thelangostudent/Models/master/ShipModel/" + selectedModel + ".gltf"));
+            sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
+            startActivity(sceneViewerIntent);
+        }
+    }
 
 
+    public void updatePicture(Context context){
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        String text = mySpinner.getSelectedItem().toString();
 
+        String imageResultURL = "none";
+        switch (text) {
+            case "Ship":
+                imageResultURL = "shipPicture.png";
+                break;
+            case "Player":
+                imageResultURL = "player.png";
+                break;
+            case "Asteroid":
+                imageResultURL = "asteroid.png";
+                break;
+            case "Forest Village Planet":
+                imageResultURL = "forestVillagePlanet.png";
+                break;
+            case "Select model":
+                imageResultURL = "none.png";
+                break;
+        }
 
+        ImageView previewImage=(ImageView) findViewById(R.id.previewImage);
+
+        Glide.with(context)
+                .load("https://raw.githubusercontent.com/Thelangostudent/Models/master/Pictures/" + imageResultURL)
+                .into(previewImage);
     }
 }

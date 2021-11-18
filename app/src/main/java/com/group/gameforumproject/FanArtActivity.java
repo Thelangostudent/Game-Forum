bba5d8f8203277726f8b8f70827df61107b09729
Mamo-00 <mahmodahmed2000@gmail.com>
2021-11-18 20:32:45 +0100

    Did some xml editing

--- app/src/main/java/com/group/gameforumproject/FanArtActivity.java
+++ app/src/main/java/com/group/gameforumproject/FanArtActivity.java
@@ -3,9 +3,12 @@ package com.group.gameforumproject;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.Intent;
@@ -18,11 +21,24 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FanArtActivity extends AppCompatActivity {
 
     Button logout;
     FloatingActionButton create_post;
+    RecyclerView fanArtPostRecyclerView ;
+    FanArtPostAdapter fanArtPostAdapter ;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference ;
+    List<FanArtPost> fanArtPostList;
 
 
     @Override
@@ -46,6 +62,46 @@ public class FanArtActivity extends AppCompatActivity {
         create_post.setOnClickListener(view -> {
             Intent create_fan_art_intent = new Intent(FanArtActivity.this, CreateFanArtPost.class);
             startActivity(create_fan_art_intent);
+        });
+
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Get List Posts from the database
+        fanArtPostRecyclerView  = findViewById(R.id.fanArtRecyclerview);
+        fanArtPostRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        fanArtPostRecyclerView.setHasFixedSize(true);
+        firebaseDatabase = FirebaseDatabase.getInstance("https://game-forum-backend-default-rtdb.europe-west1.firebasedatabase.app/");
+        databaseReference = firebaseDatabase.getReference("Posts");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                fanArtPostList = new ArrayList<>();
+                for (DataSnapshot postsnap: dataSnapshot.getChildren()) {
+
+                    FanArtPost post = postsnap.getValue(FanArtPost.class);
+                    fanArtPostList.add(post) ;
+
+
+
+                }
+
+                fanArtPostAdapter = new FanArtPostAdapter(FanArtActivity.this,fanArtPostList);
+                fanArtPostRecyclerView.setAdapter(fanArtPostAdapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
         });
 
 

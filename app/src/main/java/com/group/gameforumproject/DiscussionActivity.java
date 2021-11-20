6faa70365f831f1f5c7e7f84939f074148b07de5
Thelango99 <sanderrf@stud.ntnu.no>
2021-11-20 23:37:38 +0100

    Created a test to push DiscussionPosts to FireBase.

--- app/src/main/java/com/group/gameforumproject/DiscussionActivity.java
+++ app/src/main/java/com/group/gameforumproject/DiscussionActivity.java
@@ -9,7 +9,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -31,6 +34,9 @@ public class DiscussionActivity extends AppCompatActivity {
     private DiscussionPostAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private FloatingActionButton addNewDiscssionPost;
+    FirebaseDatabase remoteDataBase;
+    DatabaseReference referenceData;
+
     Button logoutButton;
 
     @Override
@@ -60,10 +66,9 @@ public class DiscussionActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.discussionRecyclerview);
 
-        adapter = new DiscussionPostAdapter(discussionPostList,DiscussionActivity.this);
+        adapter = new DiscussionPostAdapter(discussionPostList, DiscussionActivity.this);
 
         recyclerView.setHasFixedSize(true);
-
 
 
         layoutManager = new LinearLayoutManager(this);
@@ -80,13 +85,19 @@ public class DiscussionActivity extends AppCompatActivity {
             }
         });
 
-
+        for (DiscussionPost post : discussionPostList)
+        {
+            addPost(post);
+        }
     }
 
 
-    private void dummyPostEntries()
-    {
-        DiscussionPost d1 = new DiscussionPost(0,"When is the game releasing?","Just wanted to know k thnx", commentList, user);
+
+
+
+
+    private void dummyPostEntries() {
+        DiscussionPost d1 = new DiscussionPost(0, "When is the game releasing?", "Just wanted to know k thnx", commentList, user);
         DiscussionPost d2 = new DiscussionPost(1, "LOL looking forward to your game", "What the title said", commentList, user);
         DiscussionPost d3 = new DiscussionPost(2, "I must Alpha test!", "Am on my sigma grind, need to test a game", commentList, user);
 
@@ -95,4 +106,30 @@ public class DiscussionActivity extends AppCompatActivity {
         discussionPostList.add(d3);
 
     }
+
+    // this is just a test method lifted from CreateFanPosts. DO NOT USE FOR FINAL IMPLEMENTATION.
+    private void addPost(DiscussionPost discussionPost) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://game-forum-backend-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference("DiscussionPostBeta").push();
+
+        // get post unique ID and update post key
+
+
+        // add post data to firebase database
+
+        myRef.setValue(discussionPost).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                //showMessage("Post Added successfully");
+
+                //Intent mainIntent = new Intent(CreateFanArtPost.this, FanArtActivity.class);
+                //startActivity(mainIntent);
+            }
+        });
+    }
+
+
 }
+
+
